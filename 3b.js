ef5dc537bc5d302da692d2cb369b6337ec10bb73
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-10-06 19:38:12 +0300

    Support unblocking autoplay

--- 3b.js
+++ 3b.js
@@ -3,19 +3,35 @@ window.__register_yt3b = () => {
 		setTimeout(window.__register_yt3b, 50);
 		return;
 	} else {
+    	window.__video = document.querySelector("video");
 		console.info("3B||Registered!");
 	};
-	document.querySelector("video").addEventListener("pause", () => {
-	    if(document.querySelector("video").currentTime<5){
-	    	console.info("3B||YT trying to pause video");
-			document.querySelector("video").play();
+	window.__video.addEventListener("pause", () => {
+	    if(window.__video.currentTime<5){
+	    	console.info("3B||YT trying to pause video. (Playing..)");
+			window.__video.play();
 	    };
+	    
+	    
 	    if(!!document.querySelector("tp-yt-iron-overlay-backdrop")){
 			document.querySelector("tp-yt-iron-overlay-backdrop").remove();
 			document.querySelector("ytd-popup-container").remove();
-			console.info("3B||YT trying to show blocker popup");
-			document.querySelector("video").play();
+			console.info("3B||YT trying to show blocker popup. (Removing..)");
+			window.__video.play();
 	    };
+	    
+	    if(window.__video.duration==window.__video.currentTime){
+	        setTimeout(() => {
+	            if(!document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number")) return;
+                window.__prev_timeout = Number(document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number").innerText);
+                setTimeout(()=>{
+                    if(Number(document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number")?.innerText)==window.__prev_timeout){
+                        console.info("3B||YT trying to block autoplaying. (Skipping..)");
+                        document.querySelector(".ytp-autonav-endscreen-upnext-button.ytp-autonav-endscreen-upnext-play-button.ytp-autonav-endscreen-upnext-button-rounded").click();
+                    };
+                },Number(document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number").innerText)*1000 - 500);
+            }, 250);
+        }
 	});
 };
 
