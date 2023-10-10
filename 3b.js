4947591589d7fdb49a7274fa298b5340b2ed5d04
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-10-10 21:08:18 +0300

    Change entire system

--- 3b.js
+++ 3b.js
@@ -7,38 +7,24 @@ window.__register_yt3b = () => {
 		console.info("3B||Registered!");
 	};
 		
-	window.__video.addEventListener("pause", () => {
-	    if(!!document.querySelector("tp-yt-iron-overlay-backdrop")){
-			if(!document.querySelector(`div#microphone:is([state="listening"],[state="speaking"])`)){
-				window.__video.play();
-				document.querySelector("tp-yt-iron-overlay-backdrop").remove();
-				document.querySelector("ytd-popup-container").remove();
-				console.info("3B||YT trying to show blocker popup. (Removing..)");
-				document.body.append(document.createElement("ytd-popup-container"));
-				console.info("3B||Restoring <ytd-popup-container> for showing unrelated dialogs");
-			} else {
-				console.info("3B||YT is listening your microphone (for searching?) and video will be stay paused");
-			};
-	    };
-	    
-	    if(window.__video.duration==window.__video.currentTime){
-	        setTimeout(() => {
-	            if(!document.querySelector(".ytp-autonav-endscreen-upnext-button.ytp-autonav-endscreen-upnext-play-button.ytp-autonav-endscreen-upnext-button-rounded") || document.querySelector(".ytp-autonav-toggle-button").getAttribute("aria-checked") == "false") return;
-	            if(!document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number")){
-	                window.__prev_timeout = -9;
-	            } else {
-	                window.__prev_timeout = Number(document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number").innerText);
-	            };
-	            
-                setTimeout(()=>{
-                    if(window.__prev_timeout == -9 || Number(document.querySelector(".ytp-autonav-endscreen-upnext-header-countdown-number")?.innerText)==window.__prev_timeout){
-                        console.info("3B||YT trying to block autoplaying. (Skipping..)");
-                        document.querySelector(".ytp-autonav-endscreen-upnext-button.ytp-autonav-endscreen-upnext-play-button.ytp-autonav-endscreen-upnext-button-rounded").click();
-                    };
-                }, window.__prev_timeout * 1000 - 500);
-            }, 250);
+	window.__video.addEventListener("durationchange", () => {        
+        if(window.__video.duration < 15) {
+            console.info("3B||Unskippable AD Removed!");
+            window.__video.currentTime = window.__video.duration;
+        };
+	});
+	
+	window.__video.addEventListener("timeupdate", () => {
+	    if(!!document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text")){
+            console.info("3B||AD Removed!");
+            document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text").click();
         };
 	});
 };
 
 setTimeout(window.__register_yt3b, 50);
+document.head.insertAdjacentHTML("beforeend",`<style>
+#sparkles-container, yt-mealbar-promo-renderer, #player-ads, ytd-ad-slot-renderer, ytd-page-top-ad-layout-renderer, ytd-ad-engagement-panel-banner-renderer, *[target-id="engagement-panel-ads"] {
+	display: none !important;
+}
+</style>`);
--- README.md
+++ README.md
@@ -1,18 +1,27 @@
-# YouTube | Block AdBlock Blocker
+# YouTube | AdBlocker
 
-Use YouTube with your adblock and without useless adblock warnings.
-
-https://github.com/malisipi/yt-3b/assets/87281783/b23c9225-d2a5-475a-bd42-16e9c630fc9c
-
-> Note: This extension never block any ad (it just blocks adblock warnings). So it designed for using with your external adblock. You can use this project with adblocker such as [uBO](https://github.com/gorhill/uBlock).
+Use YouTube without useless ads!
 
 ## Features
 
-* Remove useless adblock warnings and auto-pauses.
+* Remove ADs without YT detects it :D
 
 ## Disclaimer
 
 > Using the extension can cause many sanctions up to the closure of your YouTube and Google account. Use the extension in your responsibility.
+
+## FAQ
+
+### Why named as YT|3B?
+
+> The reason of that is first design idea. Firstly, it designed block adblocker warnings. However, later, it was changed to an anblock. 3B name is coming from `Block adBlocker Blocker`.
+
+### What changed?
+
+> YT was improved their AD system. So the extension was affected badly. So I was turned it an adblock since it's easier than breaking AD system.
+
+* After `0.1.0`, it's an adblock.
+* `0.1.0` and before `0.1.0`, it's a helper to use YT with adblock without useless warnings.
 
 ## License
 
